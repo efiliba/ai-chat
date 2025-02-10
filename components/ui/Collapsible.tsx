76ef27f9ab1d503ae89a8918cdae685155c5c9de
Eli Filiba <efiliba@hotmail.com>
2025-02-10 18:48:59 +1100

    Animate Collapsible

--- components/ui/Collapsible.tsx
+++ components/ui/Collapsible.tsx
@@ -1,11 +1,15 @@
 "use client"
 
+import { type PropsWithChildren } from "react"
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
+
+import "./Collapsible.css";
 
 const Collapsible = CollapsiblePrimitive.Root
 
 const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
 
-const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent
+const CollapsibleContent = (props: PropsWithChildren<{ className: string }>) =>
+  <CollapsiblePrimitive.CollapsibleContent {...props} className={`collapsible-content ${props.className}`} />
 
 export { Collapsible, CollapsibleTrigger, CollapsibleContent }
