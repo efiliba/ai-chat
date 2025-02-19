6553ceba30939278b75468aacab485785d27b2ef
Eli Filiba <efiliba@hotmail.com>
2025-02-19 16:10:23 +1100

    Revert to using scroll anchor instead of flex col reverse to auto scroll responses

--- app/page.tsx
+++ app/page.tsx
@@ -22,39 +22,39 @@ export default function Home() {
   };
 
   return (
-    <div className="grid grid-rows-[1fr_auto] h-screen">
-      <div className="container flex flex-col-reverse max-w-4xl p-4 mx-auto overflow-y-auto">
-        <div className="space-y-4 basis-full">
-          {history.map(({ error, cancelled, role, content }, index) =>
-            role === "user" ? (
-              <UserQuestion
-                key={index}
-                error={error}
-                cancelled={cancelled}
-                text={content}
-              />
-            ) : (
-              <AIResponse
-                key={index}
-                error={error}
-                cancelled={cancelled}
-                reasoning={content.reasoning}
-                answer={content.answer}
-                hideReasoning={hideReasoning}
-              />
-            )
-          )}
-          <AIResponse
-            error={error}
-            loading={loading}
-            reasoning={reasoning}
-            answer={answer}
-            hideReasoning={hideReasoning}
-            onToggleReasoning={handleToggleReasoning}
-          />
-        </div>
+    <div className="[&_*:not(.scroll-anchor)]:[overflow-anchor:none] min-h-screen grid grid-rows-[1fr_auto_auto]">
+      <div className="container max-w-4xl p-4 mx-auto space-y-4">
+        {history.map(({ error, cancelled, role, content }, index) =>
+          role === "user" ? (
+            <UserQuestion
+              key={index}
+              error={error}
+              cancelled={cancelled}
+              text={content}
+            />
+          ) : (
+            <AIResponse
+              key={index}
+              error={error}
+              cancelled={cancelled}
+              reasoning={content.reasoning}
+              answer={content.answer}
+              hideReasoning={hideReasoning}
+            />
+          )
+        )}
+        <AIResponse
+          error={error}
+          loading={loading}
+          reasoning={reasoning}
+          answer={answer}
+          hideReasoning={hideReasoning}
+          onToggleReasoning={handleToggleReasoning}
+        />
       </div>
+      <div className="scroll-anchor h-[1px]" />
       <QuestionInput
+        className="sticky bottom-0"
         loading={loading}
         focus={!loading}
         onSubmit={handleLoadAIResponse}
